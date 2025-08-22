dbdcc78a1b869aa9f316862b79893dc8b4bde0ba
Ketbome <Pablo.moraga.san@gmail.com>
2025-08-21 21:36:00 -0400

    feat: implement geolocation support in LocationsComponent and update resource request handling

--- src/app/domains/info/pages/locations/locations.component.ts
+++ src/app/domains/info/pages/locations/locations.component.ts
@@ -1,4 +1,4 @@
-import { Component, resource } from '@angular/core';
+import { afterNextRender, Component, resource, signal } from '@angular/core';
 import { environment } from '@env/environment';
 
 @Component({
@@ -7,9 +7,28 @@ import { environment } from '@env/environment';
   templateUrl: './locations.component.html',
 })
 export default class LocationsComponent {
+  $origin = signal('');
+
+  constructor() {
+    afterNextRender(() => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+        const origin = `${position.coords.latitude},${position.coords.longitude}`;
+        this.$origin.set(origin);
+      });
+    });
+  }
+
   locationRs = resource({
-    loader: async () => {
-      const response = await fetch(`${environment.apiUrl}/api/v1/locations`);
+    request: () => ({
+      origin: this.$origin(),
+    }),
+    loader: async ({ request }) => {
+      const url = new URL(`${environment.apiUrl}/api/v1/locations`);
+      if (request.origin) {
+        url.searchParams.set('origin', request.origin);
+      }
+      const response = await fetch(url);
       const data = await response.json();
       return data;
     },
